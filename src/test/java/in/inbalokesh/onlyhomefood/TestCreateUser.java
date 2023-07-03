d10afcc528c525c808483906a9a7041161571f99
Inbalokesh <inbalokesh.esakkipandi@fwsa.freshworks.com>
2023-07-03 11:08:35 +0530

    updated test case

--- src/test/java/in/inbalokesh/onlyhomefood/TestCreateUser.java
+++ src/test/java/in/inbalokesh/onlyhomefood/TestCreateUser.java
@@ -33,7 +33,7 @@ public class TestCreateUser {
 		Exception exception = assertThrows(Exception.class,()->{
 			userService.create(null);
 		});
-		String expectedMessage = "Invalid Input";
+		String expectedMessage = "Invalid User Input";
 		
 		String receivedMessage = exception.getMessage();
 		
@@ -46,14 +46,42 @@ public class TestCreateUser {
 
 		User newUser = new User();
 		newUser.setId(12345);
-		newUser.setEmail("inba@gmail.com");
+		newUser.setEmail(null);
 		newUser.setFirstName("Inba");
 		newUser.setLastName("Lokesh");
 		newUser.setPassword("14");
 		newUser.setActive(true);
 		
-		assertDoesNotThrow(()->{
+		Exception exception = assertThrows(Exception.class,()->{
 			userService.create(newUser);
-		});	
+		});
+		
+		String expectedMessage = "Email cannot be null or empty";
+		
+		String receivedMessage = exception.getMessage();
+		
+		assertTrue(expectedMessage.equals(receivedMessage));
+	}
+	
+	@Test
+	public void testCreateUserWithEmailEmpty() {
+		Exception exception = assertThrows(Exception.class,()->{
+			UserService userService = new UserService();
+
+			User newUser = new User();
+			newUser.setId(12345);
+			newUser.setEmail("");
+			newUser.setFirstName("Inba");
+			newUser.setLastName("Lokesh");
+			newUser.setPassword("14");
+			newUser.setActive(true);
+			
+			userService.create(newUser);
+		});
+		String expectedMessage = "Email cannot be null or empty";
+		
+		String receivedMessage = exception.getMessage();
+		
+		assertTrue(expectedMessage.equals(receivedMessage));
 	}
 }
